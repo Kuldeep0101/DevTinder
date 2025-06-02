a4e11808be1257d9761ef1e71fcea67f439cb909
Kuldeep0101 <kuldeepsingh44682@gmail.com>
2025-06-02 20:16:53 +0530

    Added HTTP API Routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello there");
+const data = { Name: "Kuldeep", Stack: "MERN" };
+
+app.use("/user", (req, res) => {
+  res.send("You cant go ahead");
+});
+
+app.get("/user", (req, res) => {
+  res.status(200).json({ message: "Data Fetched", data: data });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Data Saved to DB ");
+  res.send({ message: "User Saved to DB" });
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User Deleted");
 });
 
 app.listen(8000, () => console.log("Server running on port 8000"));
