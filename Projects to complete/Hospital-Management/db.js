9bb782bdddaf0d9ac717cadc9bd63b8c0b31df3a
Kuldeep0101 <141560186+Kuldeep0101@users.noreply.github.com>
2025-08-19 19:53:35 +0530

    Deleted unncessery folders

--- Projects to complete/Hospital-Management/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: "localhost", // Default host
-  port: 3306, // Default port
-  database: "hospital-db", // Replace with the name of your database
-});
-
-module.exports = connection;
--- Projects to complete/Hospital-Management/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("welcome to Homepage");
-});
-
-app.listen(9000, () => console.log("runnnn"));
